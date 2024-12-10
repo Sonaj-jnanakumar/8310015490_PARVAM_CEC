2ed5f8d999eacd99697109f55b68e503547c27a4
Sonaj-jnanakumar <jkamalaj1983@gmail.com>
2024-12-10 12:29:54 +0530

    Add files via upload

---
+++ pointers3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int size=0,a[100],i,*small=a;
+    scanf("%d",&size);
+    for(i=0;i<size;scanf("%d",a+i++));
+    for(i=1;i<size;i++)
+    {
+        if(*small > a[i])
+        small=&a[i];
+    }
+    printf("Smallest Element is %d",*small);
+    return 0;
+
+}
